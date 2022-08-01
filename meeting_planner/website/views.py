0cf0f08572d1e0123e66408abd9418638fd563a0
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-08-01 23:33:13 +0300

    adding template variable

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+    {"message": "This data was sent from the view to the template" })
 
 def date(request):
     return HttpResponse("This page was served at: " + str(datetime.now()))
