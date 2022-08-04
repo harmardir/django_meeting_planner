9e3b72e3cfa3c6da6c93918b02432ae68e482aa6
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-08-04 14:10:06 +0300

    moving urls inside apps

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 from website.views import welcome , date , about
 from meetings.views import detail, rooms_list
@@ -9,6 +9,5 @@ urlpatterns = [
     path('welcome/', welcome , name = 'welcome'),
     path('date/', date),
     path('about/', about),
-    path('meetings/<int:id>',detail , name='detail'),
-    path('rooms', rooms_list, name='rooms'),
+    path('meetings/', include('meetings.urls')),
 ]
